ef51f3ba4c6da2f9cf1e64363897bea5c88b6a64
Piermaria Cosina <piermaria.cosina@gmail.com>
2013-01-30 18:40:03 +0100

    clean up

--- tacati/functions.php
+++ tacati/functions.php
@@ -35,7 +35,7 @@ add_action( 'after_setup_theme', 'tacati_child_load_languages' );
 
 if ( !function_exists( 'bp_dtheme_enqueue_styles' ) ) :
 	function wp_enqueue_styles(){
-		$version = '0.2';
+		$version = '0.1';
 		
 		wp_register_style( 'screen',  get_theme_root_uri() . '/css/screen.css', array(), $version, 'screen, projection' );
 		wp_register_style( 'print',  get_theme_root_uri() . '/css/print.css', array(), $version, 'print' );
